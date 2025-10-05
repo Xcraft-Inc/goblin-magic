3610c73150e59404f707241d1eda690028944a57
Mathieu Schroeter <mathieu@schroetersa.ch>
2025-10-05 14:25:30 +0200

    Add test for moveTab

---
+++ test/navigation.spec.js
@@ -0,0 +1,129 @@
+'use strict';
+
+const {expect} = require('chai');
+const {Elf} = require('xcraft-core-goblin/lib/test.js');
+
+describe('goblin.magic.navigation', function () {
+  const {
+    MagicNavigationLogic,
+  } = require('../widgets/magic-navigation/service.js');
+
+  function restore(navLogic) {
+    navLogic.change('windows', {
+      'win@1': {
+        panelIds: ['panel@1', 'panel@2'],
+        dialogIds: ['dialog@1'],
+        activePanelId: 'panel@1',
+      },
+    });
+    navLogic.change('panels', {
+      'panel@1': {
+        tabIds: ['tab@1', 'tab@2', 'tab@3'],
+        lastTabIds: [],
+      },
+      'panel@2': {
+        tabIds: ['tab@4', 'tab@5', 'tab@6'],
+        lastTabIds: [],
+      },
+    });
+    navLogic.change('tabs', {
+      'tab@1': {
+        highlighted: false,
+      },
+      'tab@2': {
+        highlighted: false,
+      },
+      'tab@3': {
+        highlighted: false,
+      },
+      'tab@4': {
+        highlighted: false,
+      },
+      'tab@5': {
+        highlighted: false,
+      },
+      'tab@6': {
+        highlighted: false,
+      },
+    });
+  }
+
+  it('moveTab', function () {
+    const navLogic = Elf.trial(MagicNavigationLogic);
+    navLogic.create('magicNavigation@test');
+
+    // --- one panel -----------------------------------------------------------
+
+    /* 1,2,3 → 2,3,1 */
+    restore(navLogic);
+    navLogic.moveTab('panel@1', 'tab@1', 'panel@1', 'tab@3', 'right');
+    expect(navLogic.state.panels['panel@1'].tabIds.toJS()) //
+      .to.be.eql(['tab@2', 'tab@3', 'tab@1']);
+
+    /* 1,2,3 → 2,1,3 */
+    restore(navLogic);
+    navLogic.moveTab('panel@1', 'tab@1', 'panel@1', 'tab@3', 'left');
+    expect(navLogic.state.panels['panel@1'].tabIds.toJS()) //
+      .to.be.eql(['tab@2', 'tab@1', 'tab@3']);
+
+    /* 1,2,3 → 2,1,3 */
+    restore(navLogic);
+    navLogic.moveTab('panel@1', 'tab@1', 'panel@1', 'tab@2', 'right');
+    expect(navLogic.state.panels['panel@1'].tabIds.toJS()) //
+      .to.be.eql(['tab@2', 'tab@1', 'tab@3']);
+
+    /* 1,2,3 → 1,2,3 */
+    restore(navLogic);
+    navLogic.moveTab('panel@1', 'tab@1', 'panel@1', 'tab@2', 'left');
+    expect(navLogic.state.panels['panel@1'].tabIds.toJS()) //
+      .to.be.eql(['tab@1', 'tab@2', 'tab@3']);
+
+    /* 1,2,3 → 3,1,2 */
+    restore(navLogic);
+    navLogic.moveTab('panel@1', 'tab@3', 'panel@1', 'tab@1', 'left');
+    expect(navLogic.state.panels['panel@1'].tabIds.toJS()) //
+      .to.be.eql(['tab@3', 'tab@1', 'tab@2']);
+
+    /* 1,2,3 → 1,3,2 */
+    restore(navLogic);
+    navLogic.moveTab('panel@1', 'tab@3', 'panel@1', 'tab@1', 'right');
+    expect(navLogic.state.panels['panel@1'].tabIds.toJS()) //
+      .to.be.eql(['tab@1', 'tab@3', 'tab@2']);
+
+    // --- two panels ----------------------------------------------------------
+
+    /* 2,3 → 4,5,6,1 */
+    restore(navLogic);
+    navLogic.moveTab('panel@1', 'tab@1', 'panel@2', 'tab@6', 'right');
+    expect(navLogic.state.panels['panel@1'].tabIds.toJS()) //
+      .to.be.eql(['tab@2', 'tab@3']);
+    expect(navLogic.state.panels['panel@2'].tabIds.toJS()) //
+      .to.be.eql(['tab@4', 'tab@5', 'tab@6', 'tab@1']);
+
+    /* 1,3 → 2,4,5,6 */
+    restore(navLogic);
+    navLogic.moveTab('panel@1', 'tab@2', 'panel@2', 'tab@4', 'left');
+    expect(navLogic.state.panels['panel@1'].tabIds.toJS()) //
+      .to.be.eql(['tab@1', 'tab@3']);
+    expect(navLogic.state.panels['panel@2'].tabIds.toJS()) //
+      .to.be.eql(['tab@2', 'tab@4', 'tab@5', 'tab@6']);
+
+    /* 1,2 → 4,5,3,6 */
+    restore(navLogic);
+    navLogic.moveTab('panel@1', 'tab@3', 'panel@2', 'tab@5', 'right');
+    expect(navLogic.state.panels['panel@1'].tabIds.toJS()) //
+      .to.be.eql(['tab@1', 'tab@2']);
+    expect(navLogic.state.panels['panel@2'].tabIds.toJS()) //
+      .to.be.eql(['tab@4', 'tab@5', 'tab@3', 'tab@6']);
+
+    /* . → 2,4,1,5,6,3 */
+    restore(navLogic);
+    navLogic.moveTab('panel@1', 'tab@2', 'panel@2', 'tab@4', 'left');
+    navLogic.moveTab('panel@1', 'tab@1', 'panel@2', 'tab@5', 'left');
+    navLogic.moveTab('panel@1', 'tab@3', 'panel@2', 'tab@6', 'right');
+    expect(navLogic.state.panels['panel@1'].tabIds.toJS()) //
+      .to.be.eql([]);
+    expect(navLogic.state.panels['panel@2'].tabIds.toJS()) //
+      .to.be.eql(['tab@2', 'tab@4', 'tab@1', 'tab@5', 'tab@6', 'tab@3']);
+  });
+});
